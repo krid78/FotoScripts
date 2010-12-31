1268787f373bc6b3c5e8c884c8fbf9a423b5b75e
Daniel Kriesten <krid@tu-chemnitz.eu>
2010-12-31 18:09:39 +0100

    Bugfixes und Erweiterungen nach erstem Probelauf
    Signed-off-by: Daniel Kriesten <krid@tu-chemnitz.eu>

--- pics2folder.sh
+++ pics2folder.sh
@@ -25,20 +25,25 @@ function copyPicsToFolder ()
 	local i
 	local x
 	for i in $(find "${SRCBASE}" -iname "*.${1}"); do
-		local FILENAME=$(basename "${i}")
 		getTimeStamp "${i}";
 		if [[ "x"${YEAR}${MONTH}${DAY} == "x00000000" ]]; then
 			echo "Skipping \"${i}\""
 			continue
 		fi
 		createFolder "${DSTBASE}"/${YEAR}/${YEAR}-${MONTH}-${DAY}
-		if [[ -e "${DSTBASE}/${YEAR}/${YEAR}-${MONTH}-${DAY}/${YEAR}${MONTH}${DAY}_${FILENAME}" ]]; then
-			echo "** \"${DSTBASE}/${YEAR}/${YEAR}-${MONTH}-${DAY}/${YEAR}${MONTH}${DAY}_${FILENAME}\" existiert! **"
-		else
-			for x in ${i%.*}.*; do
-				cp -iv "$x" "${DSTBASE}/${YEAR}/${YEAR}-${MONTH}-${DAY}"/${YEAR}${MONTH}${DAY}_"${FILENAME}"
-			done
-		fi
+		for x in ${i%.*}.*; do
+			local FILENAME=$(basename "${x}")
+			local NAME=${FILENAME%.*}
+			local EXT=$(echo ${FILENAME##*.} |tr "[:upper:]" "[:lower:]")
+			if [[ ${EXT} != "jpg" ]]; then
+				EXT=${FILENAME##*.}
+			fi
+			if [[ -e "${DSTBASE}/${YEAR}/${YEAR}-${MONTH}-${DAY}/${YEAR}${MONTH}${DAY}_${NAME}.${EXT}" ]]; then
+				echo "** \"${DSTBASE}/${YEAR}/${YEAR}-${MONTH}-${DAY}/${YEAR}${MONTH}${DAY}_${NAME}.${EXT}\" existiert! **"
+			else
+				cp -iv "$x" "${DSTBASE}/${YEAR}/${YEAR}-${MONTH}-${DAY}"/${YEAR}${MONTH}${DAY}_"${NAME}.${EXT}"
+			fi
+		done
 	done
 }
 
@@ -47,39 +52,51 @@ function movePicsToFolder ()
 	local i
 	local x
 	for i in $(find "${SRCBASE}" -iname "*.${1}"); do
-		local FILENAME=$(basename "${i}")
 		getTimeStamp "${i}";
 		if [[ "x"${YEAR}${MONTH}${DAY} == "x00000000" ]]; then
 			echo "Skipping \"${i}\""
 			continue
 		fi
 		createFolder "${DSTBASE}"/${YEAR}/${YEAR}-${MONTH}-${DAY}
-		if [[ -e "${DSTBASE}/${YEAR}/${YEAR}-${MONTH}-${DAY}/${YEAR}${MONTH}${DAY}_${FILENAME}" ]]; then
-			echo "** \"${DSTBASE}/${YEAR}/${YEAR}-${MONTH}-${DAY}/${YEAR}${MONTH}${DAY}_${FILENAME}\" existiert! **"
-		else
-			for x in ${i%.*}.*; do
-				mv -iv "$x" "${DSTBASE}/${YEAR}/${YEAR}-${MONTH}-${DAY}"/${YEAR}${MONTH}${DAY}_"${FILENAME}"
-			done
-		fi
+		for x in ${i%.*}.*; do
+			local FILENAME=$(basename "${x}")
+			local NAME=${FILENAME%.*}
+			local EXT=$(echo ${FILENAME##*.} |tr "[:upper:]" "[:lower:]")
+			if [[ ${EXT} != "jpg" ]]; then
+				EXT=${FILENAME##*.}
+			fi
+			if [[ -e "${DSTBASE}/${YEAR}/${YEAR}-${MONTH}-${DAY}/${YEAR}${MONTH}${DAY}_${NAME}.${EXT}" ]]; then
+				echo "** \"${DSTBASE}/${YEAR}/${YEAR}-${MONTH}-${DAY}/${YEAR}${MONTH}${DAY}_${NAME}.${EXT}\" existiert! **"
+			else
+				mv -iv "$x" "${DSTBASE}/${YEAR}/${YEAR}-${MONTH}-${DAY}"/${YEAR}${MONTH}${DAY}_"${NAME}.${EXT}"
+			fi
+		done
 	done
 }
 
+####################
+# adapt the PATH and DYLD_LIBRARY_PATH
+####################
+
+export PATH=$PATH:/Volumes/MY\ PASSPORT/Pictures/Fotos/Tools
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Volumes/MY\ PASSPORT/Pictures/Fotos/Tools
+
 SRCBASE=${1:-.}
-if [[ -d ${SRCBASE} ]]; then
-	pushd ${SRCBASE}
-	SRCBASE=${PWD}
+if [[ -d "${SRCBASE}" ]]; then
+	pushd "${SRCBASE}"
+	SRCBASE="${PWD}"
 	popd
-	echo "SRCBASE=${SRCBASE}"
+	echo "SRCBASE=\"${SRCBASE}\""
 else
 	exit
 fi
 
 DSTBASE=${2:-./Pictures}
-if [[ -d ${DSTBASE} ]]; then
-	pushd ${DSTBASE}
-	DSTBASE=${PWD}
+if [[ -d "${DSTBASE}" ]]; then
+	pushd "${DSTBASE}"
+	DSTBASE="${PWD}"
 	popd
-	echo "DSTBASE=${DSTBASE}"
+	echo "DSTBASE=\"${DSTBASE}\""
 else
 	exit
 fi
@@ -87,8 +104,8 @@ fi
 BASEEXT=${3:-jpg}
 echo "BASEEXT=${BASEEXT}"
 
-#copyPicsToFolder "${BASEEXT}"
-movePicsToFolder "${BASEEXT}"
+copyPicsToFolder "${BASEEXT}"
+#movePicsToFolder "${BASEEXT}"
 
 # vim: ts=2:sw=2:tw=0:fileformat=unix
 # vim: comments& comments+=b\:# formatoptions& formatoptions+=or
