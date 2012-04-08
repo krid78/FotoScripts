3282d8eac470de502a478c6987db10f65c8d7234
Daniel Kriesten <krid@tu-chemnitz.eu>
2012-04-08 20:17:19 +0200

    widely rewritten
    option parser
    help message
    minor improvements
    
    Signed-off-by: Daniel Kriesten <krid@tu-chemnitz.eu>

--- pics2folder.sh
+++ pics2folder.sh
@@ -5,126 +5,166 @@
 # Debugging
 #set -x
 
-# host dependent settings for the behaviour
-case "$HOSTNAME" in
-	macup*)
-		MOVE=yes
-		DATETIME=
-		#MOVE=1
-		#DATETIME=yes
-		;;
-	*)
-		echo "No color for $HOSTNAME"
-		MOVE=
-		DATETIME=
-		#MOVE=1
-		#DATETIME=yes
-		;;
-esac
+#########################
+# functions
+function getCleanDirPath()
+{
+  local CDP
+  local __result=$1
+  pushd "${2}" >& /dev/null
+  CDP="${PWD}"
+  popd >& /dev/null
+  eval $__result="'$CDP'"
+}
 
 function getTimeStamp ()
 {
-	echo "Trying Exif.Photo.DateTimeDigitized"
-	local  date=$(exiv2 -q -Pv -g Exif.Photo.DateTimeDigitized "${1}" |awk '{print $1}'|tr ':' ' ')
-	local	ptime=$(exiv2 -q -Pv -g Exif.Photo.DateTimeDigitized "${1}" |awk '{print $2}'|tr ':' ' ')
-	if [[ "x"$date = "x" ]]; then
-		echo "No Exif.Photo.DateTimeDigitized; trying Exif.Photo.DateTimeOriginal"
-		date=$(exiv2 -q -Pv -g Exif.Photo.DateTimeOriginal "${1}" |awk '{print $1}'|tr ':' ' ')
-		ptime=$(exiv2 -q -Pv -g Exif.Photo.DateTimeOriginal "${1}" |awk '{print $2}'|tr ':' ' ')
-	fi
-	if [[ "x"$date = "x" ]]; then
-		echo "No Exif.Photo.DateTimeOriginal; trying Exif.Image.DateTime;"
-		date=$(exiv2 -q -Pv -g Exif.Image.DateTime "${1}" |awk '{print $1}'|tr ':' ' ')
-		ptime=$(exiv2 -q -Pv -g Exif.Image.DateTime "${1}" |awk '{print $2}'|tr ':' ' ')
-	fi
-	if [[ "x"$date = "x" ]]; then
-		echo "***** No exif data! *****"
-		date="0000 00 00"
-		ptime="00 00 00"
-	fi
-	read YEAR MONTH DAY <<<${date}
-	read HOUR MINUTE SECOND <<<${ptime}
+  echo "Trying Exif.Photo.DateTimeDigitized"
+  local pdate
+  local ptime
+  read pdate ptime <<<"$(exiv2 -q -Pv -g Exif.Photo.DateTimeDigitized "${1}")"
+  if [[ "x"$pdate = "x" ]]; then
+    echo "No Exif.Photo.DateTimeDigitized; trying Exif.Photo.DateTimeOriginal"
+    read pdate ptime <<<"$(exiv2 -q -Pv -g Exif.Photo.DateTimeOriginal "${1}")"
+  fi
+  if [[ "x"$pdate = "x" ]]; then
+    echo "No Exif.Photo.DateTimeOriginal; trying Exif.Image.DateTime;"
+    read pdate ptime <<<"$(exiv2 -q -Pv -g Exif.Image.DateTime "${1}")"
+  fi
+  if [[ "x"$pdate = "x" ]]; then
+    echo "***** No exif data! *****"
+    #TODO: use file date & time
+    pdate="0000:00:00"
+    ptime="00:00:00"
+  fi
+  read YEAR MONTH DAY <<<$(echo ${pdate}|tr ':' ' ')
+  read HOUR MINUTE SECOND <<<$(echo ${ptime}|tr ':' ' ')
 }
 
 function createFolder ()
 {
-	mkdir -vp "${1}"
+  mkdir ${VOPT} -p "${1}"
 }
 
 function copyPicsToFolder ()
 {
-	local i
-	local x
-	find "${SRCBASE}" -iname "*.${1}"|while read i; do
-	getTimeStamp "${i}";
-	if [[ "x"${YEAR}${MONTH}${DAY} == "x00000000" ]]; then
-		echo "Skipping \"${i}\""
-		continue
-	fi
-	createFolder "${DSTBASE}"/${YEAR}/${YEAR}-${MONTH}-${DAY}
-	for x in "${i%.*}".*; do
-		local FILENAME=$(basename "${x}")
-		local NAME=${FILENAME%.*}
-		local EXT=$(echo ${FILENAME##*.} |tr "[:upper:]" "[:lower:]")
-		if [[ ${EXT} != "jpg" ]]; then
-			EXT=${FILENAME##*.}
-		fi
-		if [[ "x"${DATETIME} != "x" ]]; then
-			if [[ -e "${DSTBASE}/${YEAR}/${YEAR}-${MONTH}-${DAY}/${YEAR}${MONTH}${DAY}_${HOUR}${MINUTE}${SECOND}_${NAME}.${EXT}" ]]; then
-				echo "** \"${DSTBASE}/${YEAR}/${YEAR}-${MONTH}-${DAY}/${YEAR}${MONTH}${DAY}_${HOUR}${MINUTE}${SECOND}_${NAME}.${EXT}\" existiert! **"
-			else
-				${CPCOMMAND} "$x" "${DSTBASE}/${YEAR}/${YEAR}-${MONTH}-${DAY}"/${YEAR}${MONTH}${DAY}_${HOUR}${MINUTE}${SECOND}_"${NAME}.${EXT}"
-			fi
-		else
-			if [[ -e "${DSTBASE}/${YEAR}/${YEAR}-${MONTH}-${DAY}/${YEAR}${MONTH}${DAY}_${NAME}.${EXT}" ]]; then
-				echo "** \"${DSTBASE}/${YEAR}/${YEAR}-${MONTH}-${DAY}/${YEAR}${MONTH}${DAY}_${NAME}.${EXT}\" existiert! **"
-			else
-				${CPCOMMAND} "$x" "${DSTBASE}/${YEAR}/${YEAR}-${MONTH}-${DAY}"/${YEAR}${MONTH}${DAY}_"${NAME}.${EXT}"
-			fi
-		fi
-	done
-done
+  local i
+  local x
+  find "${SRCDIR}" -iname "*.${1}"|while read i; do
+    getTimeStamp "${i}";
+    if [[ "x"${YEAR}${MONTH}${DAY} == "x00000000" ]]; then
+      echo "Skipping \"${i}\""
+      continue
+    fi
+    createFolder "${DSTDIR}"/${YEAR}/${YEAR}-${MONTH}-${DAY}
+    for x in "${i%.*}".*; do
+      local FILENAME=$(basename "${x}")
+      local NAME=${FILENAME%.*}
+      local EXT=$(echo ${FILENAME##*.} |tr "[:upper:]" "[:lower:]")
+      if [[ ${EXT} != "jpg" ]]; then
+        EXT=${FILENAME##*.}
+      fi
+      if [[ "x"${DATETIME} != "x" ]]; then
+        if [[ -e "${DSTDIR}/${YEAR}/${YEAR}-${MONTH}-${DAY}/${YEAR}${MONTH}${DAY}_${HOUR}${MINUTE}${SECOND}_${NAME}.${EXT}" ]]; then
+          echo "** \"${DSTDIR}/${YEAR}/${YEAR}-${MONTH}-${DAY}/${YEAR}${MONTH}${DAY}_${HOUR}${MINUTE}${SECOND}_${NAME}.${EXT}\" existiert! **"
+        else
+          ${CPCOMMAND} ${VOPT} "$x" "${DSTDIR}/${YEAR}/${YEAR}-${MONTH}-${DAY}"/${YEAR}${MONTH}${DAY}_${HOUR}${MINUTE}${SECOND}_"${NAME}.${EXT}"
+        fi
+      else
+        if [[ -e "${DSTDIR}/${YEAR}/${YEAR}-${MONTH}-${DAY}/${YEAR}${MONTH}${DAY}_${NAME}.${EXT}" ]]; then
+          echo "** \"${DSTDIR}/${YEAR}/${YEAR}-${MONTH}-${DAY}/${YEAR}${MONTH}${DAY}_${NAME}.${EXT}\" existiert! **"
+        else
+          ${CPCOMMAND} ${VOPT} "$x" "${DSTDIR}/${YEAR}/${YEAR}-${MONTH}-${DAY}"/${YEAR}${MONTH}${DAY}_"${NAME}.${EXT}"
+        fi
+      fi
+    done
+  done
 }
 
-####################
-# adapt the PATH and DYLD_LIBRARY_PATH
-####################
+#########################
+# initialize variables
+VERSION=0.9
+CPCOMMAND="cp -i"
+VOPT=
+getCleanDirPath SRCDIR "./"
+DSTDIR="${HOME}/Pictures"
+DATETIME=
+BASEEXT="jpg"
 
-export PATH=$PATH:/Volumes/MY\ PASSPORT/Pictures/Fotos/Tools
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Volumes/MY\ PASSPORT/Pictures/Fotos/Tools
+#########################
+# Main
+NAME=$(basename $0)
+USAGE="== ${NAME}; Version: ${VERSION} ==\n\
+\n\
+Usage: $0 <options> \n\
+\n\
+Options:\n\
+  -s <dir>\t Source Directory (default: ${SRCDIR})\n\
+  -d <dir>\t Destiantion Directory (default: ${DSTDIR})\n\
+  -e <ext>\t Extension for Files (default: ${BASEEXT}\n\
+  -m \t\t Move files (default is to copy them) \n\
+  -t \t\t Use date and time to name the files \n\
+  -v \t\t Be verbose \n\
+  -h \t\t This Help \n"
 
-SRCBASE=${1:-.}
-if [[ -d "${SRCBASE}" ]]; then
-	pushd "${SRCBASE}"
-	SRCBASE="${PWD}"
-	popd
-	echo "SRCBASE=\"${SRCBASE}\""
-else
-	exit
+if [[ $# -lt 1 ]]; then 
+  echo -e $USAGE
+  exit 0
 fi
 
-DSTBASE=${2:-./Pictures}
-if [[ -d "${DSTBASE}" ]]; then
-	pushd "${DSTBASE}"
-	DSTBASE="${PWD}"
-	popd
-	echo "DSTBASE=\"${DSTBASE}\""
-else
-	exit
+while getopts "hvs:d:e:mt" options; do
+  #echo "Option: $options"
+  case $options in
+    m)
+      CPCOMMAND="mv -i"
+      ;;
+    s)
+      if [[ -d "${OPTARG}" ]]; then
+        getCleanDirPath SRCDIR "${OPTARG}"
+      else
+        echo "Not a Directory: ${OPTARG}!"
+        exit 2
+      fi
+      ;;
+    d)
+      if [[ -d "${OPTARG}" ]]; then
+        getCleanDirPath DSTDIR "${OPTARG}"
+      else
+        echo "Not a Directory: ${OPTARG}!"
+        exit 2
+      fi
+      ;;
+    e)
+      BASEEXT="${OPTARG}"
+      ;;
+    t)
+      DATETIME="yes"
+      ;;
+    v)
+      VOPT="-v"
+      ;;
+    h)
+      echo -e $USAGE
+      exit 0
+      ;;
+    *)
+      echo -e $USAGE
+      exit 1
+      ;;
+  esac
+done
+
+if [[ ! -z $VOPT ]]; then
+  echo -e "Settings: \n\
+    CPCOMMAND=${CPCOMMAND} \n\
+    VOPT=${VOPT} \n\
+    SRCDIR=${SRCDIR} \n\
+    DSTDIR=${DSTDIR} \n\
+    DATETIME=${DATETIME} \n\
+    BASEEXT=${BASEEXT} \n"
 fi
-
-BASEEXT=${3:-jpg}
-echo "BASEEXT=${BASEEXT}"
-
-if [[ "x"${MOVE} != "x" ]]; then
-	CPCOMMAND="mv -iv"
-else
-	CPCOMMAND="cp -iv"
-fi
-echo "CP COMMAND=${CPCOMMAND}"
-
 
 copyPicsToFolder "${BASEEXT}"
 
-# vim: ts=2:sw=2:tw=0:fileformat=unix
+# vim: ts=2:sts=2:sw=2:expandtab:fileformat=unix
 # vim: comments& comments+=b\:# formatoptions& formatoptions+=or
